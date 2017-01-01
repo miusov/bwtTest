667d636542e01555603e66ae6ee32a0827b9d066
miusov <miusov86@gmail.com>
2017-01-01 22:10:06 +0200

    01.01.17v4

--- template/footer.php
+++
@@ -1,8 +0,0 @@
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.5/js/bootstrap.min.js"></script>
-    <script src="/bwtTest/template/js/jquery.validate.min.js"></script>
-    <script src="/bwtTest/template/js/main.js"></script>
-</body>
-</html>
